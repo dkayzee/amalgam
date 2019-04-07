a8f2bb9076439c7a2f159f97a94f5b70ae204716
Daniel Kim <daniel.seungchul.kim@gmail.com>
2019-04-07 12:45:33 -0400

    something something

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './HomePage/HomePage.js'
+import TaskEdit from './TaskView/TaskView.js'
 
 class App extends Component {
+  state = {
+    objectId: null
+  }
+
+  liftingObjectId = (value) => {
+    this.setState({objectId:value})
+  }
+
   render() {
     return (
       <div className="App_container">
-        <HomePage />
+        <main>
+          <Switch>
+            <Route path='/' exact component={HomePage} />
+            <Route path='/:id' exact render={props =>{
+              return <TaskEdit objectID={props.match.params.id} />
+            }} />
+          </Switch>
+
+        </main>
       </div>
     );
   }
--- src/Components/RecentFiles/RecentFiles.js
+++ src/Components/RecentFiles/RecentFiles.js
@@ -1,13 +1,73 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 import './RecentFiles.css'
 
+import axios from 'axios'
+
+const URL = "https://yuuvis.io/api/dms/objects/search"
+
 class RecentFiles extends Component{
+    constructor(){
+        super()
+        this.state = {
+            recentObjects: null,
+            isLoaded: false,
+            clickedObjectId: null
+        }
+    }
+
+    fetchRecentObjects = async() => {
+        const resp = await axios({
+            method: 'POST',
+            headers: {
+                "X-ID-TENANT-NAME": "nyc058",
+                "accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": "Basic YWRtaW46eVduV3RKSllKR0lJ"
+            },
+            data: {
+                query: {
+                "statement": "SELECT * FROM enaio:object ORDER BY enaio:lastModificationDate DESC",
+                "skipCount": 0,
+                "maxItems": 8
+                }
+            },
+            url: URL
+        })
+        this.setState({recentObjects: resp.data.objects, isLoaded: true})
+    }
+
+    componentDidMount(){
+        this.fetchRecentObjects()
+    }
+
+    // handleClick = (id) => {
+    //     console.log("ui work")
+    //     return <Redirect to={`/${id}`} />
+    // }
+
     render(){
-        return(
-            <div className="RecentFiles_container">
-                Recent Files
-            </div>
-        )
+        const renderRecentFiles = this.state.isLoaded ? this.state.recentObjects.map(object => {
+            return (
+                <div className="">
+                    <span>{object.properties["enaio:objectId"].value}</span>
+                    <span>{object.properties["Name"].value}</span>
+                    <button onClick={() => this.setState({clickedObjectId: object.properties["enaio:objectId"].value})}>View Document</button>
+                </div>
+            ) 
+        }) : null
+
+        if(this.state.clickedObjectId === null){
+            return(
+                <div className="RecentFiles_wrapper">
+                    <div className="RecentFiles_header"><h1>Recent Files</h1></div>
+                    <div className="RecentFiles_container">{renderRecentFiles}</div>
+                </div>
+            )
+        }
+        else{
+            return <Redirect to={`/${this.state.clickedObjectId}`} />
+        }
     }
 }
 
